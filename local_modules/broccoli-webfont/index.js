3fc8873521019223a5f6256e85c3f54a0f86f2b5
apezel <arnaud.pezel@me.com>
2026-08-06 22:06:24 +0200

    test

---
+++ local_modules/broccoli-webfont/index.js
@@ -0,0 +1,36 @@
+var path = require('path')
+var _ = require('underscore')
+var Q = require('q')
+var CachingWriter = require('broccoli-glob-caching-writer')
+var webfontsGenerator = require('webfonts-generator')
+
+function Webfont(inputTree, options) {
+	if (!options) options = {}
+	if (!options.files) options.files = ['**/*.svg']
+	if (!(this instanceof Webfont)) return new Webfont(inputTree, options)
+	CachingWriter.apply(this, arguments)
+}
+
+Webfont.prototype = Object.create(CachingWriter.prototype)
+Webfont.prototype.description = 'Webfont'
+
+/** Generates webfonts in the 'destDir'. */
+Webfont.prototype.updateCache = function(srcDir, destDir, files) {
+	if (!files.length) return
+
+	var webfontsOptions = _.extend({}, this.options)
+	_.each(['dest', 'cssDest', 'htmlDest'], function(option) {
+		var value = this.options[option]
+		if (value !== undefined) webfontsOptions[option] = path.join(destDir, value)
+	}, this)
+	webfontsOptions.files = _.map(files, function(file) {
+	 return path.join(srcDir, file)
+	})
+	if (webfontsOptions.dest === undefined) webfontsOptions.dest = destDir
+
+	return Q.nfcall(webfontsGenerator, webfontsOptions)
+}
+
+Webfont.templates = webfontsGenerator.templates
+
+module.exports = Webfont
